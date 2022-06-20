0ab0d44bcc8e364b65b10028237fd3c3f1ce845b
brunopintos <brunopintos98@gmail.com>
2022-06-20 14:31:17 -0300

    added test should not remove addres that is not admin

--- test/vault-test.js
+++ test/vault-test.js
@@ -7,53 +7,58 @@ let vault;
 const [wallet, walletTo, thirdWallet] = provider.getWallets();
 
 describe("Vault", () => {
+  beforeEach(async () => {
+    vault = await deployContract(wallet, Vault, []);
+  });
 
-    beforeEach(async () => {
-        vault = await deployContract(wallet, Vault, []);
+  describe("Constructor", () => {
+    it("should deploy the contract with deployer as admin", async () => {
+      expect(await vault.admins(wallet.address)).to.equal(true);
+    });
+  });
+
+  describe("Admin", () => {
+    it("should be able to add a new admin if you are admin", async () => {
+      // Act
+      await vault.addAdmin(walletTo.address);
+      // Assert
+      expect(await vault.admins(walletTo.address)).to.equal(true);
     });
 
-    describe("Constructor", () => {
-        it("should deploy the contract with deployer as admin", async () => {
-            expect(await vault.admins(wallet.address)).to.equal(true);
-        })
+    it("should not be able to add a new admin if you are not admin", async () => {
+      // Arrange
+      const vaultFromAnotherAccount = vault.connect(walletTo);
+      // Assert
+      await expect(vaultFromAnotherAccount.addAdmin(walletTo.address)).to.be
+        .reverted;
     });
 
-    describe("Admin", () => {
-        it("should be able to add a new admin if you are admin", async () => {
-            // Act
-            await vault.addAdmin(walletTo.address);
-            // Assert
-            expect(await vault.admins(walletTo.address)).to.equal(true);
-        });
-
-        it("should not be able to add a new admin if you are not admin", async () => {
-            // Arrange
-            const vaultFromAnotherAccount = vault.connect(walletTo);
-            // Assert
-            await expect(vaultFromAnotherAccount.addAdmin(walletTo.address)).to.be.reverted;
-        })
-
-        it("should be able to remove admin if you are admin", async () => {
-            // Arrange
-            await vault.addAdmin(walletTo.address);
-            // Act        
-            await vault.removeAdmin(walletTo.address);
-            // Assert
-            expect(await vault.admins(walletTo.address)).to.equal(false);
-        });
-
-        it("should not be able to remove an admin if you are not admin", async () => {
-            // Arrange
-            await vault.addAdmin(walletTo.address);
-            const vaultFromNotAdminAccount = vault.connect(thirdWallet);
-            // Assert                 
-            await expect(vaultFromNotAdminAccount.removeAdmin(wallet.address)).to.be.reverted;
-        });
-
-        it("should not be able to remove last admin", async () => {
-            await expect(vault.removeAdmin(wallet.address)).to.be.reverted;
-        });
-
+    it("should be able to remove admin if you are admin", async () => {
+      // Arrange
+      await vault.addAdmin(walletTo.address);
+      // Act
+      await vault.removeAdmin(walletTo.address);
+      // Assert
+      expect(await vault.admins(walletTo.address)).to.equal(false);
     });
 
+    it("should not be able to remove an admin if you are not admin", async () => {
+      // Arrange
+      await vault.addAdmin(walletTo.address);
+      const vaultFromNotAdminAccount = vault.connect(thirdWallet);
+      // Assert
+      await expect(vaultFromNotAdminAccount.removeAdmin(wallet.address)).to.be
+        .reverted;
+    });
+
+    it("should not be able to remove last admin", async () => {
+      // Assert
+      await expect(vault.removeAdmin(wallet.address)).to.be.reverted;
+    });
+
+    it("should not be able to remove an address that is not an admin", async () => {
+      // Assert
+      await expect(vault.removeAdmin(walletTo.address)).to.be.reverted;
+    });
+  });
 });
